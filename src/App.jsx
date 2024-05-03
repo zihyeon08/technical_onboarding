7fce3727012af25437da1d31c56181be135167b9
Jihyeon Hwang <104641422+zihyeon08@users.noreply.github.com>
2024-05-03 15:22:14 +0000

    make my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "jihyeon Hwang",
+  title: "DevRel Dreamer & Content Creator",
+  email: "JIHYEON.HWANG@studentambassadors.com",
+  gitHub: "@zihyeon08",
+  instagram: "@zihy_o3",
+  linkedIn: "@jihyeon081",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/gray.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "gray color simple image ";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "My name is Jihyeon Hwang, and I am majoring in computer science at Kyungpook National University. I'm interested in GitHub, Blazor, and Power Platform.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Teaching skills",
+  "Blazor",
+  "communication skills",
 ];
 
 /**
@@ -45,7 +42,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "To become a Devrel, I am gaining experience presenting in many communities. These days, I'm in charge of creating a homepage, so I'm studying Power Pages.";
 
 const About = () => {
   return (
